5e15c6c8e9deb6877b1ac660ae8bfe6fa6aa6058
Takaogahara <fael.rlopes@gmail.com>
2020-07-23 18:13:43 -0300

    Keras Tuner update

---
+++ Image_Processing/Semiconductor_CV/0.0.1/Semiconductor_CV.py
@@ -0,0 +1,98 @@
+__author__ = "Rafael Lopes Almeida"
+__maintainer__ = "Rafael Lopes Almeida"
+__email__ = "fael.rlopes@gmail.com"
+
+__date__ = "23/07/2020"
+__version__ = "0.0.1"
+
+import utils
+import numpy as np
+import os
+
+import tensorflow as tf
+from tensorflow import keras
+from keras.preprocessing.image import ImageDataGenerator
+from kerastuner import RandomSearch
+from kerastuner.engine.hyperparameters import HyperParameters
+
+# from PIL import Image
+# import matplotlib.pyplot as plt
+
+print(f'Tensorflow version: {tf.__version__}')
+print(f'Keras version: {keras.__version__}')
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(gpus[0], True)
+
+print(f'TF config completed')
+
+# ----------------------------------------------------------------------------------- Folder Path
+
+general_path = ('D:/Code/Source/Semiconductor CV/Images/')
+train_path = ('D:/Code/Source/Semiconductor CV/Images/Train/')
+test_path = ('D:/Code/Source/Semiconductor CV/Images/Test/')
+
+# ----------------------------------------------------------------------------------- Parameters
+
+image_count_train = sum(len(files) for _, _, files in os.walk(train_path))
+image_count_test = sum(len(files) for _, _, files in os.walk(test_path))
+CLASS_NAMES = np.array([name for name in os.listdir(train_path) 
+                                if os.path.isdir(general_path)])
+
+BATCH_SIZE = 16
+TARGET_SIZE = (224, 224)
+STEPS_PER_EPOCH_TRAIN = np.ceil(image_count_train/BATCH_SIZE)
+STEPS_PER_EPOCH_TEST = np.ceil(image_count_test/BATCH_SIZE)
+
+# ----------------------------------------------------------------------------------- IMG Generator
+
+datagen = ImageDataGenerator(rescale=1/255)
+
+train_images_gen = datagen.flow_from_directory(
+        train_path, 
+        batch_size=BATCH_SIZE, target_size=TARGET_SIZE, 
+        shuffle=True, classes=list(CLASS_NAMES))
+
+
+test_images_gen = datagen.flow_from_directory(
+        test_path, batch_size=BATCH_SIZE, 
+        target_size=TARGET_SIZE, shuffle=True)
+
+# -----------------------------------------------------------------------------------
+
+tuner_image, tuner_label = utils.get_mini_batch(train_images_gen)
+# utils.show_batch(tuner_image, tuner_label, CLASS_NAMES)d
+
+# ----------------------------------------------------------------------------------- Keras Tuner
+
+tuner_search = RandomSearch(
+        utils.build_model, max_trials=5, 
+        objective='val_accuracy',project_name='Kerastuner', 
+        directory='D:/Code/Source/Semiconductor CV/')
+
+
+tuner_search.search(
+        train_images_gen, 
+        epochs=3, verbose=2,
+        validation_data = test_images_gen)
+
+#  tuner_search.search(
+#         tuner_image, tuner_label, 
+#         epochs=3,validation_split=0.1, 
+#         verbose=2)       
+
+
+model = tuner_search.get_best_models(num_models=1)[0]
+model.summary()
+
+# -----------------------------------------------------------------------------------
+
+# model.save_weights('D:/Code/Source/Semiconductor CV/CNN_weights.h5')
+
+# -----------------------------------------------------------------------------------
+
+model.fit(train_images_gen,epochs=5,
+        steps_per_epoch=STEPS_PER_EPOCH_TRAIN,
+        validation_data=test_images_gen,
+        validation_steps=STEPS_PER_EPOCH_TEST)
+ 
--- Image_Processing/Semiconductor_CV/0.0.1/utils.py
+++ Image_Processing/Semiconductor_CV/0.0.1/utils.py
@@ -1,3 +1,10 @@
+__author__ = "Rafael Lopes Almeida"
+__maintainer__ = "Rafael Lopes Almeida"
+__email__ = "fael.rlopes@gmail.com"
+
+__date__ = "23/07/2020"
+__version__ = "0.0.1"
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -38,26 +45,33 @@ def get_mini_batch(image_generator):
 def build_model(hp):
     model = keras.Sequential([
         keras.layers.Conv2D(
-            filters = hp.Int('conv_1_filter', min_value = 32, max_value = 128, step = 16),   
+            filters = hp.Int('conv_1_filter', min_value = 32, max_value = 80, step = 16),   
             kernel_size = hp.Choice('conv_1_kernel', values = [3,5,7]),                
             activation = 'relu',
-            input_shape = (224, 224, 3)
-        ),
+            input_shape = (224, 224, 3)),
+        keras.layers.MaxPooling2D(pool_size=(2)),
+
         keras.layers.Conv2D(
             filters = hp.Int('conv_2_filter', min_value = 32, max_value = 128, step = 16),
             kernel_size = hp.Choice('conv_2_kernel', values = [3,5,7]),
-            activation = 'relu'
-        ),
+            activation = 'relu'),
+        keras.layers.MaxPooling2D(pool_size=(2)),
+
+        keras.layers.Conv2D(
+            filters = hp.Int('conv_3_filter', min_value = 32, max_value = 256, step = 16),
+            kernel_size = hp.Choice('conv_3_kernel', values = [3,5,7]),
+            activation = 'relu'),
+        keras.layers.MaxPooling2D(pool_size=(2)),
+
         keras.layers.Flatten(),
         keras.layers.Dense(
-            units = hp.Int('dense_1_units', min_value = 32, max_value = 128, step = 16),
-            activation = 'relu'
-        ),
-        keras.layers.Dense(2, activation = 'softmax')     
-        ])
+            units = hp.Int('dense_1_units', min_value = 32, max_value = 256, step = 16),
+            activation = 'relu'),
+
+        keras.layers.Dense(1, activation = 'sigmoid')])#softmax
 
     model.compile(optimizer = keras.optimizers.Adam(hp.Choice('learning_rate', values = [1e-2, 1e-3, 1e-4])),
-                  loss = 'sparse_categorical_crossentropy',
+                  loss = 'binary_crossentropy', #sparse_categorical_crossentropy
                   metrics = ['accuracy'])
     
     return model
